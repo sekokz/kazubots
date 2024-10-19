529478779c41b7139888e8b7ca1c6c1cb262db91
kazu <kz.seko10@gmail.com>
2024-10-20 05:04:21 +0900

    custom

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ from discord.abc import GuildChannel
 from discord.flags import Intents
 from datetime import datetime
 import os
+import random
 from server import server_thread
 
 DISCORD_TOKEN = os.environ['DISCORD_TOKEN']
@@ -98,8 +99,61 @@ async def on_message(message):
     await channel.send(msg, silent=True)
 
   elif mode == 4:
-    await channel.send(f"開発予定の機能です...まさかハッキングされた！？", silent=True)
+
+    AZs = []
+    TZs = []
+    BZs = []
+
+    # 全加入者の一覧を総当たり
+    for m in message.guild.members:
+      rolenames = []
+      # 正規メンバーだけを対象とする
+      for role in m.roles:
+        # その人の全ロールを配列にする
+        rolenames.append(role.name)
+        # メンバーである
+        if rolenames.count("メンバー") > 0:
+          # かつポジション
+          if rolenames.count("AZ") > 0:
+            AZs.append(m.global_name)
+          elif rolenames.count("TZ") > 0:
+            TZs.append(m.global_name)
+          elif rolenames.count("BZ") > 0:
+            BZs.append(m.global_name)
+
+    # シャッフルする
+    role_limits = {'AZ':4, 'TZ':3, 'BZ':2}
+    initial_roles = [(mem, 'AZ') for mem in AZs] + [(mem, 'TZ') for mem in TZs] + [(mem, 'BZ') for mem in BZs]
+
+    new_roles = shuffle_roles(initial_roles, role_limits)
+
+    msg = '今日のポジションは...これでどうかな！？\n'
+
+    for mem, new_role in new_roles:
+      msg += f"{mem}: {new_role}\n"
+
+    await channel.send(msg, silent=True)
+
+def shuffle_roles(initial_roles, role_limits):
+  # 新しいリスト
+  shuffled_roles = []
+
+  # 各役割がシャッフル後に就ける役割
+  candidates = {'AZ': ['TZ', 'BZ'], 'TZ': ['AZ', 'BZ'], 'BZ': ['AZ', 'TZ']}
+
+  # 各役割の人数カウント
+  role_count = {'AZ': 0, 'TZ': 0, 'BZ': 0}
+
+  for mem, role in initial_roles:
+    # 人数を超えないように割り当てる
+    available_roles = [r for r in candidates[role] if role_count[r] < role_limits[r]]
+    new_role = random.choice(available_roles)
+
+    # 新しい役割を追加してカウントを更新
+    shuffled_roles.append((mem, new_role))
+    role_count[new_role] += 1
+
+  return shuffled_roles
 
 server_thread()
 client.run(DISCORD_TOKEN)
-
